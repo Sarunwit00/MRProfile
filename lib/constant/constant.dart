fc626ef1a81603230020064b268175441c546775
SHXBX <natdanai.workspace@gmail.com>
2024-12-24 16:09:13 +0700

    v1

---
+++ lib/constant/constant.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+const bgPrimaryColor = Colors.black;
+const bgSecondaryColor = Color.fromARGB(255, 33, 243, 208);
+
+const textPrimaryColor = Colors.white;
+const textSecondary = Color.fromARGB(255, 247, 184, 103);
+
+const iconPrimary = Colors.amber;
+const iconSecondary = Colors.white;
+
+final texttitle = GoogleFonts.poppins(
+  fontSize: 20,
+  fontWeight: FontWeight.w600,
+  color: textPrimaryColor,
+);
+
+final textSubTitle = GoogleFonts.poppins(
+  fontSize: 14,
+  fontWeight: FontWeight.w100,
+  color: textPrimaryColor,
+);
---
+++ lib/screens/profile_screen.dart
@@ -0,0 +1,28 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Center(
+            child: CircleAvatar(
+              backgroundColor: Colors.blue,
+              radius: 100,
+              child: CircleAvatar(
+                radius: 95,
+                backgroundImage: AssetImage(
+                    'images/images.jpg'), // ใช้ backgroundImage แทน backgroundColor
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
